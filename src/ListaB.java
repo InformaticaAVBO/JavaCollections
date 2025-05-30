3cd1d45aff661f02bed0946f9503f6de41f272f4
JINTATTICO <150262609+JINTATTICO@users.noreply.github.com>
2025-05-30 08:44:21 +0000

    Creazione ListaB

---
+++ src/ListaB.java
@@ -0,0 +1,73 @@
+public class ListaB<T> {
+    private static class Nodo<T> {
+        private T valore;
+        private Nodo<T> precedente;
+        private Nodo<T> successivo;
+
+        Nodo(T valore) {
+            this.valore = valore;
+        }
+    }
+
+    private Nodo<T> testa;
+    private Nodo<T> coda;
+    private int size = 0;  // Per mantenere la lunghezza aggiornata
+
+    public void add(T elemento) {
+        Nodo<T> nuovoNodo = new Nodo<>(elemento);
+        if (testa == null) {
+            testa = nuovoNodo;
+            coda = nuovoNodo;
+        } else {
+            coda.successivo = nuovoNodo;
+            nuovoNodo.precedente = coda;
+            coda = nuovoNodo;
+        }
+        size++;
+    }
+
+    public void addHead(T elemento) {
+        Nodo<T> nuovoNodo = new Nodo<>(elemento);
+        if (testa == null) {
+            testa = nuovoNodo;
+            coda = nuovoNodo;
+        } else {
+            nuovoNodo.successivo = testa;
+            testa.precedente = nuovoNodo;
+            testa = nuovoNodo;
+        }
+        size++;
+    }
+
+    public void addTail(T elemento) {
+        add(elemento);
+    }
+
+    public int getLength() {
+        return size;
+    }
+
+    public void removeHead() {
+        if (testa == null) return;
+        if (testa == coda) {
+            testa = null;
+            coda = null;
+        } else {
+            testa = testa.successivo;
+            testa.precedente = null;
+        }
+        size--;
+    }
+
+    public void removeTail() {
+        if (coda == null) return;
+        if (testa == coda) {
+            testa = null;
+            coda = null;
+        } else {
+            coda = coda.precedente;
+            coda.successivo = null;
+        }
+        size--;
+    }
+}
